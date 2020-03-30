d07a531018a8e8dcf9430b5aa2196788d9723a6b
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-03-30 18:12:08 +0200

    expression label; entry

--- start.py
+++ start.py
@@ -8,7 +8,16 @@ root.geometry('300x300')
 
 # TODO sidebar to choose the Calculator template
 
-standard = Frame(root).pack()
+standard = Frame(root)
+
+expression = Label(standard, text='Exp: 3434 + 56 / 5 =', anchor='e', borderwidth=3)
+expression.grid(row=0, column=0)
+entry = Entry(standard, justify=RIGHT)
+entry.grid(row=1, column=0)
 
 
+
+
+
+standard.pack()
 root.mainloop()
