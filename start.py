c25d1e32c62c61b4cc9e7e96989d933da04eac2f
AngeStan <61012709+AngeStan@users.noreply.github.com>
2020-03-30 17:45:23 +0200

    initial root, standard frame

---
+++ start.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+version = '0.0.1'
+root.title(f'Smart Calculator {version}')
+# root.iconbitmap('')
+root.geometry('300x300')
+
+# TODO sidebar to choose the Calculator template
+
+standard = Frame(root).pack()
+
+
+root.mainloop()
